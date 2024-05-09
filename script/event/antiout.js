c8c9fd378c34a22062cb1b11bbc01e7846506a11
Mohammed1132 <162047123+Mohammed1132@users.noreply.github.com>
2024-05-09 13:10:35 +0100

    Update antiout.js

--- script/event/antiout.js
+++ script/event/antiout.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "antiout",
+  name: "السجن",
   version: "1.0.0"
 };
 module.exports.handleEvent = async ({
@@ -14,9 +14,9 @@ module.exports.handleEvent = async ({
     } = info[event.logMessageData?.leftParticipantFbId];
     api.addUserToGroup(event.logMessageData?.leftParticipantFbId, event.threadID, (error) => {
       if (error) {
-        api.sendMessage(`Unable to re-add member ${name} to the group!`, event.threadID);
+        api.sendMessage(`فشل منع العضو ${name} من المغادرة!`, event.threadID);
       } else {
-        api.sendMessage(`Active antiout mode, ${name} has been re-added to the group successfully!`, event.threadID);
+        api.sendMessage(`وضع السجن نشط, ${name} ممنوع من مغادرة المجموعة!`, event.threadID);
       }
     });
   }
